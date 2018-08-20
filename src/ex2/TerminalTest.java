4a78355d7e7504ed32a6d6a3631d9913f8c7f687
Chansik Yun <chansigi@gmail.com>
2018-08-20 16:31:33 +0900

    .

--- src/ex2/TerminalTest.java
+++ src/ex2/TerminalTest.java
@@ -1,6 +1,39 @@
 package ex2;
 
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
 public class TerminalTest {
+    private Terminal terminal;
+    @Before
+    public void setUp() throws Exception {
+        terminal = new Terminal();
+        terminal.connect();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        terminal.disconnect();
+    }
+
+
+    @Test
+    public void loginTest() throws Exception {
+        terminal.login("TEST_USER", "TEST_PASSWORD");
+
+        assertTrue(terminal.isLogin());
+    }
+
+    @Test
+    public void logoutTest() throws Exception {
+        terminal.login("TEST_USER", "TEST_PASSWORD");
+        terminal.logout();
+
+        assertFalse(terminal.isLogin());
+    }
 }
 
 
