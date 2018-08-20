fe39028e9d668fb0f7cddde326a473e81bcf3506
Chansik Yun <chansigi@gmail.com>
2018-08-20 16:26:57 +0900

    .

---
+++ src/ex2/TerminalTest.java
@@ -0,0 +1,28 @@
+package ex2;
+
+public class TerminalTest {
+}
+
+
+// SUT
+class Terminal {
+    public void connect() throws Exception {
+
+    }
+
+    public void disconnect() throws Exception {
+
+    }
+
+    public void login(String userId, String password) {
+
+    }
+
+    public void logout() {
+
+    }
+
+    public boolean isLogin() {
+        return true;
+    }
+}
