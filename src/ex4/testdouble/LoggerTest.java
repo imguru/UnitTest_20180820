86ad69ca71cb00ccbbd4b1f3e8c5c89ef5ae7e76
Chansik Yun <chansigi@gmail.com>
2018-08-21 09:26:11 +0900

    .

--- src/ex4/testdouble/LoggerTest.java
+++ src/ex4/testdouble/LoggerTest.java
@@ -1,16 +1,42 @@
 package ex4.testdouble;
 
-// 테스트 대역 목적
+// 테스트 대역 목적: 테스트 대상 코드를 격리한다.
+//   => 작성하는 테스트의 성공과 실패가 CUT에 의해서 결정되어야 한다.
+//   => 결함 국소화: 테스트가 실패할 경우 실패 지점을 바로 알 수 있다.
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
 
 public class LoggerTest {
-    
+    @Test
+    public void isValidFilename_NameLoggerThan5Chars_ReturnsTrue() {
+        String filename = "good_name.log";
+        Logger logger = new Logger();
+
+        boolean actual = logger.isValidFilename(filename);
+
+        assertTrue("다섯글자 이상일 때", actual);
+        // assertTrue("다섯글자 이상일 때", logger.isValidFilename(filename));
+    }
+
+    @Test
+    public void isValidFilename_NameShorterThan5Chars_ReturnsFalse() {
+        String filename = "ba@.log";
+        Logger logger = new Logger();
+
+        boolean actual = logger.isValidFilename(filename);
+
+        assertFalse("다섯글자 미만일 때", actual);
+    }
+
 }
 
 // SUT
 class FileSystemManager {
     public boolean isValid(String filename) {
         // NTFS, ext3, HFS
-        return true;
+        return false;
     }
 }
 
