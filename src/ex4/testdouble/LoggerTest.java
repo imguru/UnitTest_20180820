8358c30f5bb106d0985d2308627a8cda8f1a9fc4
Chansik Yun <chansigi@gmail.com>
2018-08-21 09:18:41 +0900

    .

--- src/ex4/testdouble/LoggerTest.java
+++ src/ex4/testdouble/LoggerTest.java
@@ -1,4 +1,32 @@
 package ex4.testdouble;
 
+// 테스트 대역 목적
+
 public class LoggerTest {
+    
 }
+
+// SUT
+class FileSystemManager {
+    public boolean isValid(String filename) {
+        // NTFS, ext3, HFS
+        return true;
+    }
+}
+
+class Logger {
+    // hello.log
+    //  : 파일의 확장자를 제외한 이름이 다섯 글자 이상이어야 한다.
+    public boolean isValidFilename(String filename) {
+        //--- Code Under Test ---
+        String name = filename.split("\\.")[0];
+        if (name.length() < 5)
+            return false;
+        //--- Code Under Test ---
+
+        FileSystemManager manager = new FileSystemManager();
+        return manager.isValid(filename);
+    }
+}
+
+
