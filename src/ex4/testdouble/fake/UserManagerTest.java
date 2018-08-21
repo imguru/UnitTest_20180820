50803c91dba4f870593ec700d57368da2456adcf
Chansik Yun <chansigi@gmail.com>
2018-08-21 10:27:53 +0900

    .

---
+++ src/ex4/testdouble/fake/UserManagerTest.java
@@ -0,0 +1,45 @@
+package ex4.testdouble.fake;
+
+public class UserManagerTest {
+}
+
+
+// SUT
+interface Database {
+    void save(String name, User user);
+    User load(String name);
+}
+
+// 사용자 객체(User)를 데이터베이스 저장하고, 불러오는 기능 - Helper Class
+class UserManager {
+    private Database database;
+
+    public UserManager(Database database) {
+        this.database = database;
+    }
+
+    public void create(String name, int age) {
+        if (database.load(name) != null) {
+            return;
+        }
+
+        database.save(name, new User(name, age));
+    }
+
+    public User find(String name) {
+        if (name == null) {
+            return null;
+        }
+
+        return database.load(name);
+    }
+}
+
+class User {
+
+}
+
+
+
+
+
