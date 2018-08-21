3a176eaf62d1413a17f135b1f7c3df4238111e6b
Chansik Yun <chansigi@gmail.com>
2018-08-21 11:06:47 +0900

    .

--- src/ex4/testdouble/spy/DLogTest.java
+++ src/ex4/testdouble/spy/DLogTest.java
@@ -2,3 +2,35 @@ package ex4.testdouble.spy;
 
 public class DLogTest {
 }
+
+// SUT
+enum Level {
+    INFO, WARN, ERROR
+}
+
+interface Target {
+    void write(Level level, String message);
+}
+
+// FileTarget, NetworkTarget
+class DLog {
+    private Target[] targets;
+    public DLog(Target... targets) {
+        this.targets = targets;
+    }
+
+    public void write(Level level, String message) {
+        for (Target e : targets) {
+            e.write(level, message);
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
